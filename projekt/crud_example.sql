ff7c4de2afb8824510abe76ecf8882a4f0079180
usammjekiqi <usammjekiqi@gmail.com>
2025-02-03 19:23:39 +0100

    n

--- projekt/crud_example.sql
+++ projekt/crud_example.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Jan 30, 2025 at 05:30 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Host: 127.0.0.1
+-- Generation Time: Feb 03, 2025 at 07:15 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -26,16 +26,42 @@ SET time_zone = "+00:00";
 --
 -- Table structure for table `roles`
 --
--- Error reading structure for table crud_example.roles: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table `crud_example`.`roles`
--- Error reading data for table crud_example.roles: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `crud_example`.`roles`&#039; at line 1
+
+CREATE TABLE `roles` (
+  `id` int(11) NOT NULL,
+  `role_name` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `roles`
+--
+
+INSERT INTO `roles` (`id`, `role_name`) VALUES
+(1, 'admin'),
+(2, 'User');
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `users`
 --
--- Error reading structure for table crud_example.users: #1142 - SHOW command denied to user &#039;&#039;@&#039;localhost&#039; for table `crud_example`.`users`
--- Error reading data for table crud_example.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `crud_example`.`users`&#039; at line 1
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `role_id` int(11) DEFAULT NULL,
+  `grade` varchar(10) DEFAULT 'N/A'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `role_id`, `grade`) VALUES
+(11, 'usami', '$2y$10$2XnLmPDrfNVO0VW64I7YQ..oMB5FGHpxgQx7K7D47SSs/uHDsaSru', 2, 'N/A'),
+(20, 'usam', '$2y$10$2XnLmPDrfNVO0VW64I7YQ..oMB5FGHpxgQx7K7D47SSs/uHDsaSru', 1, '100'),
+(21, 'usamm', '$2y$10$Us.d7TrshRO8N7T2nkOI.u2IeVXANu7tbhd7mGurcg46wWtLLu3Zq', 2, 'N/A');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
